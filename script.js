12c642ccc8eb0965c1b8b421fadb69e3d7dd423f
azeliahmartinez <azeliah_martinez@dlsu.edu.ph>
2024-07-13 02:19:36 +0800

    make website

---
+++ script.js
@@ -0,0 +1,19 @@
+// Back to Top button
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.querySelector('.back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            backToTopButton.classList.add('visible');
+        } else {
+            backToTopButton.classList.remove('visible');
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
